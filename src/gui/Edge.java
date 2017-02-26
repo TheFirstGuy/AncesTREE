aee085847bd626b186d34dec0112ba31382a3e75
TheFirstGuy <une202@nyu.edu>
2017-02-26 11:35:33 -0500

    Added Edge Class

--- src/gui/Edge.java
+++ src/gui/Edge.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javafx.scene.Group;
+import javafx.scene.shape.Line;
 
 /**
  * Created by Urs on 2/25/2017.
@@ -8,5 +9,38 @@ import javafx.scene.Group;
  */
 public class Edge extends Group{
 
-    //protected
+    protected Card source_;
+    protected Card target_;
+
+    private Line line_;
+
+    /**
+     * Constructor
+     * @param source Source Card to draw edge from
+     * @param target Target Card to draw edge to
+     */
+    public Edge(Card source, Card target){
+        source_ = source;
+        target_ = target;
+
+        line_ = new Line();
+
+        line_.startXProperty().bind(source_.layoutXProperty().add(source_.getBoundsInParent().getWidth() / 2.0 ));
+        line_.startYProperty().bind(source_.layoutYProperty().add(source_.getBoundsInParent().getHeight() / 2.0 ));
+
+        line_.endXProperty().bind(target_.layoutXProperty().add(target_.getBoundsInParent().getWidth() / 2.0));
+        line_.endYProperty().bind(target_.layoutYProperty().add(target_.getBoundsInParent().getHeight() / 2.0));
+
+        getChildren().add(line_);
+    }
+
+    public Card getSource(){
+        return source_;
+    }
+
+    public Card getTarget(){
+        return target_;
+    }
+
+    
 }
